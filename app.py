d2b2f14797da7a55452006e90ece140467b561e4
seong10 <kck555511@naver.com>
2022-05-19 13:09:10 +0900

    add

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import streamlit as st
 
 # 웹 대시보드 개발 라이브러리인, 스트림릿은,
 # main 함수가 있어야 한다.
+# 이것은 이렇게 해야 만들어지게 만든 틀 이다 이해가 아니라 외울부분
 
 def main() :
-    
+    st.title('안녕하세요. 웹 대시보드 프로젝트')
+    st.title('Hello')
+
+if __name__ == '__main__' :
+    main()
+
